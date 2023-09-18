c64d284cbbb86d7656f6d044be27371ace2ba4cf
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-09-18 13:00:55 +0300

    1

---
+++ hello_str.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <iostream>
+std::string itc_hello_str(std::string name)
+{
+	return "Hello, " + name;
+}
---
+++ len.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <iostream>
+long long itc_len(std::string str)
+{
+	return str.size();
+}
