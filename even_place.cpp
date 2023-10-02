188ecc2db691b832f6a482b1714c8b7c466c7548
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-02 10:54:54 +0300

    Update even_place.cpp

--- even_place.cpp
+++ even_place.cpp
@@ -2,13 +2,13 @@
 
 std::string itc_even_place(std::string str)
 {
-	std::string a;
+	std::string a = "";
 	if (itc_len(str) > 1)
 	{
 		for (int i = 1; i < itc_len(str) + 1; i++)
 		{
 			if (i % 2 == 0)
-				a.push_back(str[i - 1]);
+				a += (str[i - 1]);
 		}
 		return(a);
 	}
