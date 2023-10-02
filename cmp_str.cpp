71f0950b88d627fa76f992a89494791738910d52
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-02 10:52:41 +0300

    Update cmp_str.cpp

--- cmp_str.cpp
+++ cmp_str.cpp
@@ -1,11 +1,23 @@
 #include "str.h"
 std::string itc_cmp_str(std::string str1, std::string str2, int num)
 {
-	std::string res;
+	if (itc_len(str2) == 0)
+		return str1;
+	std::string res = "";
 	for (int i = 0; i < num; i++)
-		res.push_back(str1[i]);
-	res += str2;
-	for (int i = num; i <= itc_len(str1); i++)
-		res.push_back(str1[i]);
+		res += str1[i];
+	std::string res2 = res;
+	if(num < itc_len(str1))
+	{
+		for (int i = num; i < itc_len(str1) && itc_len(res) != itc_len(str2) + itc_len(res2); i++)
+		{
+			res += str2[i - num];
+		}
+	}
+	if(itc_len(res) < itc_len(str1))
+	{
+		for (int i = num + itc_len(str2); i <= itc_len(str1); i++)
+			res += str1[i - itc_len(str2)];
+	}
 	return res;
 }
