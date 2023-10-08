2e4fdac11e80e548a6e7b3eeebe30e5ba93c4ed7
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-08 17:47:46 +0300

    Update max_char_on_end.cpp

--- max_char_on_end.cpp
+++ max_char_on_end.cpp
@@ -2,22 +2,22 @@
 
 int itc_max_char_on_end(std::string str)
 {
-        int max = 0;
-        int current = 0;
+    int max = 0;
+    int current = 0;
 
-        for (int i = 0; i <= itc_len(str); i++)
+    for (int i = 0; i <= itc_len(str); i++)
+    {
+        if (str[i] >= '1' && str[i] <= '9')
+            current++;
+        else
         {
-            if (isdigit(str[i])) 
-                current++;
-            else 
-            {
-                if (current > max) 
-                    max = current;
-                current = 0;
-            }
+            if (current > max)
+                max = current;
+            current = 0;
         }
-        if (current > max) 
-            max = current;
-        return max;
-   
+    }
+    if (current > max)
+        max = current;
+    return max;
+
 }
