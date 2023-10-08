a8e4f9a1430b9bf4077389fa7c508031f44226f5
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-08 17:42:27 +0300

    Update three_str.cpp

--- three_str.cpp
+++ three_str.cpp
@@ -1,11 +1,17 @@
 #include "str.h"
 
-std::string itc_three_str(std::string str1, std::string str2, std::string str3)
-{
-	while(str1.find(str2) != -1)
-	{
-		int a = str1.find(str2);
-		str1.replace(a, itc_len(str2), str3);
-	}
-	return str1;
+std::string itc_three_str(std::string str1, std::string str2, std::string str3) {
+    int pos = itc_find_str(str1, str2);
+    while (pos != -1) 
+    {
+        std::string hz = "";
+        for (int i = 0; i < pos; i++)
+            hz += str1[i];
+        hz += str3;
+        for (int i = pos + itc_len(str2); i < itc_len(str1); i++)
+            hz += str1[i];
+        str1 = hz;
+        pos = itc_find_str(str1, str2);
+    }
+    return str1;
 }
