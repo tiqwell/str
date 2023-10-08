158096ae98dcd4d0d836d45a9074b78c1868eca3
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-08 16:29:03 +0300

    Update find_str.cpp

--- find_str.cpp
+++ find_str.cpp
@@ -1,6 +1,16 @@
 #include "str.h"
 
-int itc_find_str(std::string str1, std::string str2)
-{
-	return str1.find(str2);
+int itc_find_str(std::string str1, std::string str2) {
+    int len1 = itc_len(str1);
+    int len2 = itc_len(str2);
+    std::string a;
+    for (int i = 0; i <= len1 - len2; i++)
+    {
+        for (int h = 0; h < len2; h++)
+            a += str1[i + h];
+        if (a == str2)
+            return i;
+        a = "";
+    }
+    return -1;
 }
