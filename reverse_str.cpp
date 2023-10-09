d9fa78d4dd95904edfb461923fc61e1a141e6732
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-09 10:52:15 +0300

    Update reverse_str.cpp

--- reverse_str.cpp
+++ reverse_str.cpp
@@ -5,7 +5,7 @@ std::string itc_reverse_str(std::string str)
 	if (str == "")
 		return "";
 	std::string a;
-	for (int i = itc_len(str); i >= 0; i--)
-		a += (str[i] - 1);
+	for (int i = itc_len(str) - 1; i >= 0; i--)
+		a += str[i];
 	return a;
 }
