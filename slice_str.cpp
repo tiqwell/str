ff8d03ebfd96e5e458fb3f6bb90213d0c39f1a8f
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-15 13:33:17 +0300

    Update slice_str.cpp

--- slice_str.cpp
+++ slice_str.cpp
@@ -7,7 +7,7 @@ std::string itc_slice_str(std::string str, int start, int end)
 		return str;
 	else if(end > itc_len(str))
 	{
-		for (int i = start; i <= itc_len(str); i++)
+		for (int i = start; i <= itc_len(str) - 1; i++)
 			res += str[i];
 		return res;
 	}
