d4f9578b3036b5c2bec72ef23d07ed9b930a483e
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-23 10:02:51 +0300

    Update cmp_str.cpp

--- cmp_str.cpp
+++ cmp_str.cpp
@@ -7,16 +7,16 @@ std::string itc_cmp_str(std::string str1, std::string str2, int num)
 	for (int i = 0; i < num; i++)
 		res += str1[i];
 	std::string res2 = res;
-	if(num < itc_len(str1))
+	if(num < itc_len(str1) -1 )
 	{
-		for (int i = num; i < itc_len(str1) && itc_len(res) != itc_len(str2) + itc_len(res2); i++)
+		for (int i = num; i < itc_len(str1) - 1  && itc_len(res) != itc_len(str2) + itc_len(res2); i++)
 		{
 			res += str2[i - num];
 		}
 	}
-	if(itc_len(res) < itc_len(str1))
+	if(itc_len(res) - 1 < itc_len(str1) - 1)
 	{
-		for (int i = num + itc_len(str2); i <= itc_len(str1); i++)
+		for (int i = num + itc_len(str2); i < itc_len(str1); i++)
 			res += str1[i - itc_len(str2)];
 	}
 	return res;
