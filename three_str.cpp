82e80a8ef7afa3f65ee2b891bad5cdb2119466b1
tiqwell <98182406+tiqwell@users.noreply.github.com>
2023-10-23 09:43:45 +0300

    Update three_str.cpp

--- three_str.cpp
+++ three_str.cpp
@@ -1,6 +1,7 @@
 #include "str_easy.h"
 
-std::string itc_three_str(std::string str1, std::string str2, std::string str3) {
+std::string itc_three_str(std::string str1, std::string str2, std::string str3)
+{
     int pos = itc_find_str(str1, str2);
     while (pos != -1) 
     {
